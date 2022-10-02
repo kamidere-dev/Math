6e1f34b3bc0c89596c6b4a31d5f84bda62aa433a
Immanuelle <49389064+Immanuelle@users.noreply.github.com>
2022-10-01 21:13:28 -0700

    rf

---
+++ Euclidean Algorithm/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Euclidean_Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Euclidean Algorithm testing");
+        }
+    }
+}
